8bb797742b1610ac8f8727ceb335a846f7b6652b
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-01-17 12:05:00 +0530

    /Akshay initialize

---
+++ src/main/java/com/ats/rusawebapi/RusaWebapiApplication.java
@@ -0,0 +1,14 @@
+package com.ats.rusawebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RusaWebapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RusaWebapiApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/ats/rusawebapi/RusaWebapiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ats.rusawebapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RusaWebapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
