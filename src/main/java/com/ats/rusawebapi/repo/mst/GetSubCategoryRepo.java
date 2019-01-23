a9ef8078a0036677d1a9924978637b1a1a52da54
Aaryatech <code@atsinfosoft.com>
2019-01-23 12:50:59 +0530

    suCat

--- src/main/java/com/ats/rusawebapi/repo/mst/GetSubCategoryRepo.java
+++ src/main/java/com/ats/rusawebapi/repo/mst/GetSubCategoryRepo.java
@@ -10,12 +10,10 @@ import com.ats.rusawebapi.model.mst.GetSubCategory;
 
 public interface GetSubCategoryRepo extends JpaRepository<GetSubCategory, Integer>{
 	
-	
 	@Query(value="SELECT m_sub_category.*, m_category.cat_name,m_category.cat_code,m_category.cat_desc FROM"
 			+ " m_category,m_sub_category WHERE m_sub_category.cat_id=m_category.cat_id AND  m_sub_category.del_status=:delStatus",nativeQuery=true)
 
 	List<GetSubCategory> getSubAllSubCatList(@Param("delStatus") int delStatus);
-	
 	
  
 	@Query(value="SELECT m_sub_category.*, m_category.cat_name,m_category.cat_code,m_category.cat_desc FROM"
@@ -28,7 +26,6 @@ public interface GetSubCategoryRepo extends JpaRepository<GetSubCategory, Intege
 
 	List<GetSubCategory> getAllSubCatByCatId(@Param("delStatus") int delStatus,
 			@Param("catId") int catId);
-	
 	
 
 }
