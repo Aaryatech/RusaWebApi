d8faa459b42dedaf120a7316200c368075b261a9
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-01-23 11:18:58 +0530

    /Akshay

--- src/main/java/com/ats/rusawebapi/model/mst/GetSubCategory.java
+++ src/main/java/com/ats/rusawebapi/model/mst/GetSubCategory.java
@@ -36,14 +36,10 @@ public class GetSubCategory {
 	private int isActive;
 	
 	private int delStatus;
-	
-	//total 13 fields 17 Jan 2019
-	
-
+	 
 	@Transient
 	Info info;
-	
-	
+	 
 	private int exInt1;
 	private int exInt2;
 	private int exInt3;
@@ -53,18 +49,208 @@ public class GetSubCategory {
 	private String exVar3;
 
 	private String exDate1;
-	private String exDate2;
-
+	private String exDate2; 
 	private float exFloat1;
 	private float exFloat2;
-	private float exFloat3;
-	
+	private float exFloat3; 
 	private String exVar4;
-	private String exVar5;
-	//total 13 fields 18 Jan 2019
-	
+	private String exVar5; 
 	private String catName;
 	private String catCode;
 	private String catDesc;
+	public int getSubCatId() {
+		return subCatId;
+	}
+	public void setSubCatId(int subCatId) {
+		this.subCatId = subCatId;
+	}
+	public int getCatId() {
+		return catId;
+	}
+	public void setCatId(int catId) {
+		this.catId = catId;
+	}
+	public String getSubCatName() {
+		return subCatName;
+	}
+	public void setSubCatName(String subCatName) {
+		this.subCatName = subCatName;
+	}
+	public String getSubCatCode() {
+		return subCatCode;
+	}
+	public void setSubCatCode(String subCatCode) {
+		this.subCatCode = subCatCode;
+	}
+	public String getSubCatDesc() {
+		return subCatDesc;
+	}
+	public void setSubCatDesc(String subCatDesc) {
+		this.subCatDesc = subCatDesc;
+	}
+	public String getSubCatAddDate() {
+		return SubCatAddDate;
+	}
+	public void setSubCatAddDate(String subCatAddDate) {
+		SubCatAddDate = subCatAddDate;
+	}
+	public String getSubCatEditDate() {
+		return subCatEditDate;
+	}
+	public void setSubCatEditDate(String subCatEditDate) {
+		this.subCatEditDate = subCatEditDate;
+	}
+	public int getSubCatSortNo() {
+		return subCatSortNo;
+	}
+	public void setSubCatSortNo(int subCatSortNo) {
+		this.subCatSortNo = subCatSortNo;
+	}
+	public String getSubCatRemark() {
+		return subCatRemark;
+	}
+	public void setSubCatRemark(String subCatRemark) {
+		this.subCatRemark = subCatRemark;
+	}
+	public int getSectionId() {
+		return sectionId;
+	}
+	public void setSectionId(int sectionId) {
+		this.sectionId = sectionId;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public int getIsActive() {
+		return isActive;
+	}
+	public void setIsActive(int isActive) {
+		this.isActive = isActive;
+	}
+	public int getDelStatus() {
+		return delStatus;
+	}
+	public void setDelStatus(int delStatus) {
+		this.delStatus = delStatus;
+	}
+	public Info getInfo() {
+		return info;
+	}
+	public void setInfo(Info info) {
+		this.info = info;
+	}
+	public int getExInt1() {
+		return exInt1;
+	}
+	public void setExInt1(int exInt1) {
+		this.exInt1 = exInt1;
+	}
+	public int getExInt2() {
+		return exInt2;
+	}
+	public void setExInt2(int exInt2) {
+		this.exInt2 = exInt2;
+	}
+	public int getExInt3() {
+		return exInt3;
+	}
+	public void setExInt3(int exInt3) {
+		this.exInt3 = exInt3;
+	}
+	public String getExVar1() {
+		return exVar1;
+	}
+	public void setExVar1(String exVar1) {
+		this.exVar1 = exVar1;
+	}
+	public String getExVar2() {
+		return exVar2;
+	}
+	public void setExVar2(String exVar2) {
+		this.exVar2 = exVar2;
+	}
+	public String getExVar3() {
+		return exVar3;
+	}
+	public void setExVar3(String exVar3) {
+		this.exVar3 = exVar3;
+	}
+	public String getExDate1() {
+		return exDate1;
+	}
+	public void setExDate1(String exDate1) {
+		this.exDate1 = exDate1;
+	}
+	public String getExDate2() {
+		return exDate2;
+	}
+	public void setExDate2(String exDate2) {
+		this.exDate2 = exDate2;
+	}
+	public float getExFloat1() {
+		return exFloat1;
+	}
+	public void setExFloat1(float exFloat1) {
+		this.exFloat1 = exFloat1;
+	}
+	public float getExFloat2() {
+		return exFloat2;
+	}
+	public void setExFloat2(float exFloat2) {
+		this.exFloat2 = exFloat2;
+	}
+	public float getExFloat3() {
+		return exFloat3;
+	}
+	public void setExFloat3(float exFloat3) {
+		this.exFloat3 = exFloat3;
+	}
+	public String getExVar4() {
+		return exVar4;
+	}
+	public void setExVar4(String exVar4) {
+		this.exVar4 = exVar4;
+	}
+	public String getExVar5() {
+		return exVar5;
+	}
+	public void setExVar5(String exVar5) {
+		this.exVar5 = exVar5;
+	}
+	public String getCatName() {
+		return catName;
+	}
+	public void setCatName(String catName) {
+		this.catName = catName;
+	}
+	public String getCatCode() {
+		return catCode;
+	}
+	public void setCatCode(String catCode) {
+		this.catCode = catCode;
+	}
+	public String getCatDesc() {
+		return catDesc;
+	}
+	public void setCatDesc(String catDesc) {
+		this.catDesc = catDesc;
+	}
+	@Override
+	public String toString() {
+		return "GetSubCategory [subCatId=" + subCatId + ", catId=" + catId + ", subCatName=" + subCatName
+				+ ", subCatCode=" + subCatCode + ", subCatDesc=" + subCatDesc + ", SubCatAddDate=" + SubCatAddDate
+				+ ", subCatEditDate=" + subCatEditDate + ", subCatSortNo=" + subCatSortNo + ", subCatRemark="
+				+ subCatRemark + ", sectionId=" + sectionId + ", userId=" + userId + ", isActive=" + isActive
+				+ ", delStatus=" + delStatus + ", info=" + info + ", exInt1=" + exInt1 + ", exInt2=" + exInt2
+				+ ", exInt3=" + exInt3 + ", exVar1=" + exVar1 + ", exVar2=" + exVar2 + ", exVar3=" + exVar3
+				+ ", exDate1=" + exDate1 + ", exDate2=" + exDate2 + ", exFloat1=" + exFloat1 + ", exFloat2=" + exFloat2
+				+ ", exFloat3=" + exFloat3 + ", exVar4=" + exVar4 + ", exVar5=" + exVar5 + ", catName=" + catName
+				+ ", catCode=" + catCode + ", catDesc=" + catDesc + "]";
+	}
+	
+	
 
 }
