fc8850b32385d4918eae5f292a88f0f061b76fe6
Shruti Patil <Shruti Patil@192.168.10.220>
2019-01-31 00:04:55 -0500

    ///shruti

--- src/main/java/com/ats/rusawebapi/repo/BannerImagesRepository.java
+++ src/main/java/com/ats/rusawebapi/repo/BannerImagesRepository.java
@@ -1,11 +1,24 @@
 package com.ats.rusawebapi.repo;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ats.rusawebapi.model.BannerImages;
 
 public interface BannerImagesRepository extends JpaRepository<BannerImages, Integer>{
 
 	BannerImages findByIdAndDelStatus(int id, int i);
+
+	List<BannerImages> findByDelStatusOrderById(int i);
+
+	@Transactional
+	@Modifying
+	@Query("update BannerImages set del_status=0  WHERE id=:id")
+	int deleteBannerImages(int id);
 
 }
