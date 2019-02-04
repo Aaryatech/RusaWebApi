006748e2e7476e5a3715c70f101da5cfbb63bf56
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-02-04 11:31:12 +0530

    /Akshay

--- src/main/java/com/ats/rusawebapi/controller/PagesModuleApiController.java
+++ src/main/java/com/ats/rusawebapi/controller/PagesModuleApiController.java
@@ -4,13 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ats.rusawebapi.model.GetPagesModule; 
+import com.ats.rusawebapi.model.CMSPages;
+import com.ats.rusawebapi.model.GetPagesModule;
+import com.ats.rusawebapi.model.mst.Info;
+import com.ats.rusawebapi.repo.CMSPagesRepository;
 import com.ats.rusawebapi.repo.GetPagesModuleRepository;
 
 @RestController
@@ -19,6 +23,9 @@ public class PagesModuleApiController {
 	 
 	@Autowired
 	GetPagesModuleRepository getPagesModuleRepository;
+	
+	@Autowired
+	CMSPagesRepository cmsPagesRepo;
 	
 	@RequestMapping(value = { "/getCmsPagesModuleList" }, method = RequestMethod.GET)
 	public @ResponseBody List<GetPagesModule> getCmsPagesModuleList() {
@@ -55,6 +62,34 @@ public class PagesModuleApiController {
 		}
 
 		return list;
+
+	}
+	
+	@RequestMapping(value = { "/deleteCmsContent" }, method = RequestMethod.POST)
+	public @ResponseBody Info deleteCmsContent(@RequestParam("cmsPageId") int cmsPageId) {
+
+		Info errorMessage = new Info();
+		 
+		try {
+
+			int delete = cmsPagesRepo.delete(cmsPageId);
+			
+			if(delete==0) {
+				errorMessage.setError(true);
+				errorMessage.setMsg("failed to delete ");
+			}else {
+				errorMessage.setError(false);
+				errorMessage.setMsg(" deleted");
+			}
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+			errorMessage.setError(true);
+			errorMessage.setMsg("failed to delete ");
+
+		}
+		return errorMessage;
 
 	}
 
--- src/main/java/com/ats/rusawebapi/repo/CMSPagesRepository.java
+++ src/main/java/com/ats/rusawebapi/repo/CMSPagesRepository.java
@@ -1,9 +1,19 @@
 package com.ats.rusawebapi.repo;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ats.rusawebapi.model.CMSPages;
 
 public interface CMSPagesRepository extends JpaRepository<CMSPages , Integer>{
+
+	@Transactional
+	@Modifying
+	@Query("update CMSPages set del_status=0  WHERE cms_page_id=:cmsPageId")
+	int delete(@Param("cmsPageId") int cmsPageId);
 
 }
