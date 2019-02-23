cdf26c228459d8bec7477d99bab31f5f2427bf39
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-02-23 18:43:56 +0530

    /Akshay

--- src/main/java/com/ats/rusawebapi/controller/SearchRestController.java
+++ src/main/java/com/ats/rusawebapi/controller/SearchRestController.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -41,6 +43,9 @@ public class SearchRestController {
 	@Autowired
 	DocumentSearchDataRepository documentSearchDataRepository;
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
 	@RequestMapping(value = { "/serchWordFromTable" }, method = RequestMethod.POST)
 	public @ResponseBody SearchData serchWordFromTable(@RequestParam("word") String word,
 			@RequestParam("langId") int langId) {
@@ -51,7 +56,7 @@ public class SearchRestController {
 			String[] wordArray = word.split(" ");
 			List<String> wordList = new ArrayList<String>(Arrays.asList(wordArray));
 
-			 for (int i = 0; i < wordList.size(); i++) {
+			   /*for (int i = 0; i < wordList.size(); i++) {
 
 				List<CmsSearchData> cmsSerchList = cmsSearchDataRepository.cmsSerchList(wordList.get(i), langId);
 				List<FaqSearchData> faqSerchList = faqSearchDataRepository.faqSerchList(wordList.get(i), langId);
@@ -65,7 +70,7 @@ public class SearchRestController {
 					searchData.setCmsSerchList(cmsSerchList);
 					searchData.setFaqSerchList(faqSerchList);
 					searchData.setNewsSerchList(newsSerchList);
-					searchData.setGallerySerchList(gallerySerchList);
+					//searchData.setGallerySerchList(gallerySerchList);
 					searchData.setDocumentSerchList(documentSerchList);
 				}else {
 					
@@ -101,19 +106,79 @@ public class SearchRestController {
 					}
 					
 				}
-			} 
-			/*String qry1 = "SELECT cd.cms_page_id , 1 as module_id, cd.heading  , cd.page_desc , ch.add_date, ch.edit_date, t.page_name, t.page_slug from "
-					+ "t_cms_pages_description cd, t_cms_pages ch, t_pages t WHERE ";
-			String qry = new String();
-			String qry2 = " and cd.language_id="+1+" and cd.cms_page_id=ch.cms_page_id and ch.del_status=1 and ch.is_active=1 and t.page_id=ch.page_id";
+			} */
+			
+			   String cmsUpperQuery = "SELECT cd.cms_page_id , 1 as module_id, cd.heading  , cd.page_desc , ch.add_date, ch.edit_date, t.page_name, t.page_slug from "
+						+ "t_cms_pages_description cd, t_cms_pages ch, t_pages t WHERE ";
+			   String cmsBaseQuery = " and cd.language_id="+langId+" and cd.cms_page_id=ch.cms_page_id and ch.del_status=1 and ch.is_active=1 and t.page_id=ch.page_id";
+			   
+			   String faqUpperQuery = "SELECT cd.faq_id , 2 as module_id, cd.faq_que , cd.faq_ans , ch.add_date, ch.edit_date, t.page_name, t.page_slug from "
+			   		+ "t_faq_description cd, t_faq ch, t_pages t WHERE ";
+			   String faqBaseQuery = " and cd.language_id="+langId+" and cd.faq_id=ch.faq_id and ch.del_status=1 and ch.is_active=1 and t.page_id=ch.page_id";
+			   
+			   String newsUpperQuery = "SELECT cd.newsblogs_id  , 9 as module_id, cd.heading , cd.descriptions  , ch.add_date, ch.edit_date, t.page_name, "
+			   		+ "t.page_slug from t_newsblogs_description cd, t_newsblogs ch, t_pages t WHERE ";
+			   String newsBaseQuery = " and cd.language_id="+langId+" and cd.newsblogs_id=ch.newsblogs_id and ch.del_status=1 and ch.is_active=1 and t.page_id=ch.page_id";
+			   
+			   String documentUpperQuery = "SELECT cd.doc_id , 7 as module_id, cd.file_name , cd.add_date, cd.edit_date, t.page_name, t.page_slug from"
+			   		+ " t_documentupload cd, t_pages t WHERE ";
+			   String documentBaseQuery = " and cd.del_status=1 and cd.is_active=1 and t.page_id=cd.page_id";
+			   
+			String cmsMiddleQuery = new String();
+			String faqMiddleQuery = new String();
+			String newsMiddleQuery = new String();
+			String documentMiddleQuery = new String();
+			 
 			for (int i = 0; i < wordList.size(); i++) {
 
-				qry=qry+" cd.page_desc LIKE \"%"+wordList.get(i)+"%\" or  cd.heading LIKE \"%"+wordList.get(i)+"%\"  or ";
+				cmsMiddleQuery=cmsMiddleQuery+" cd.page_desc LIKE \"%"+wordList.get(i)+"%\" or  cd.heading LIKE \"%"+wordList.get(i)+"%\"  or ";
+				faqMiddleQuery=faqMiddleQuery+" cd.faq_que LIKE  \"%"+wordList.get(i)+"%\"or cd.faq_ans LIKE \"%"+wordList.get(i)+"%\" or";
+				newsMiddleQuery=newsMiddleQuery+" cd.heading LIKE  \"%"+wordList.get(i)+"%\" or cd.descriptions LIKE \"%"+wordList.get(i)+"%\" or";
+				documentMiddleQuery=documentMiddleQuery+" cd.file_name LIKE \"%"+wordList.get(i)+"%\" or";
 			}
-			qry="("+qry+"  1=2)";
-			qry=qry1+qry+qry2;
 			
-			List<CmsSearchData> cmsSerchList = cmsSearchDataRepository.cmsSerchList(qry);*/
+			cmsMiddleQuery="("+cmsMiddleQuery+"  1=2)";
+			faqMiddleQuery="("+faqMiddleQuery+"  1=2)";
+			newsMiddleQuery="("+newsMiddleQuery+"  1=2)";
+			documentMiddleQuery="("+documentMiddleQuery+"  1=2)";
+			  
+			String cmsQry=cmsUpperQuery+cmsMiddleQuery+cmsBaseQuery; 
+			List<CmsSearchData> cmsSerchList = jdbcTemplate.query(cmsQry, new BeanPropertyRowMapper(CmsSearchData.class));
+			
+			String faqQry=faqUpperQuery+faqMiddleQuery+faqBaseQuery; 
+			List<FaqSearchData> faqSerchList = jdbcTemplate.query(faqQry, new BeanPropertyRowMapper(FaqSearchData.class));
+			
+			String newsQry=newsUpperQuery+newsMiddleQuery+newsBaseQuery; 
+			List<NewsSearchData> newsSerchList = jdbcTemplate.query(newsQry, new BeanPropertyRowMapper(NewsSearchData.class));
+			
+			String docQry=documentUpperQuery+documentMiddleQuery+documentBaseQuery; 
+			List<DocumentSearchData> documentSerchList = jdbcTemplate.query(docQry, new BeanPropertyRowMapper(DocumentSearchData.class));
+			 
+			for (int i = 0; i < cmsSerchList.size(); i++) {
+				
+				StringBuilder sb = new StringBuilder(); 
+				 String nohtml = sb.append(cmsSerchList.get(i).getHeading()).toString().replaceAll("\\<.*?>","");
+				 cmsSerchList.get(i).setPageDesc(nohtml);
+			}
+			
+			for (int i = 0; i < faqSerchList.size(); i++) {
+				
+				StringBuilder sb = new StringBuilder(); 
+				 String nohtml = sb.append(faqSerchList.get(i).getFaqAns()).toString().replaceAll("\\<.*?>","");
+				 faqSerchList.get(i).setFaqAns(nohtml);
+			}
+			
+			for (int i = 0; i < newsSerchList.size(); i++) {
+				
+				StringBuilder sb = new StringBuilder(); 
+				 String nohtml = sb.append(newsSerchList.get(i).getDescriptions()).toString().replaceAll("\\<.*?>","");
+				 newsSerchList.get(i).setDescriptions(nohtml);;
+			}
+			 
+			searchData.setCmsSerchList(cmsSerchList);
+			searchData.setFaqSerchList(faqSerchList);
+			searchData.setNewsSerchList(newsSerchList);
+			searchData.setDocumentSerchList(documentSerchList);
 
 		} catch (Exception e) {
 
