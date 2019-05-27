decd308fa21f2fdd9f4c85c099a7e5325fcce4c9
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-05-27 19:13:36 +0530

    /Akshay

--- src/main/java/com/ats/rusawebapi/repo/ResultRepository.java
+++ src/main/java/com/ats/rusawebapi/repo/ResultRepository.java
@@ -11,7 +11,7 @@ public interface ResultRepository extends JpaRepository<Result, Integer>{
 
 	@Query(value="SELECT ev.newsblogs_id as id,'event-info' as class, (unix_timestamp( DATE_FORMAT(ev.event_date_from, '%Y/%m/%d 10:00:00')) * 1000 ) as start , (unix_timestamp( DATE_FORMAT(ev.event_date_from, '%Y/%m/%d 10:00:00')) * 1000 ) as end , ed.heading as title ,'#' as url       FROM `t_newsblogs` ev\n" + 
 			"inner join t_newsblogs_description ed on ev.newsblogs_id = ed.newsblogs_id \n" + 
-			"where   ed.language_id=1 and ev.ex_int1=11 and ev.event_date_from >= CURDATE();",nativeQuery=true)
+			"where   ed.language_id=1 and ev.ex_int1=11 and ev.del_status=1 and ev.event_date_from >= CURDATE();",nativeQuery=true)
 	List<Result> getEventListforCalender();
 
 	 
